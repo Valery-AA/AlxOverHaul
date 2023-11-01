9e82f4589a6aca2639eba76d2994194be13282bc
Valery-AA <valeryatart@gmail.com>
2023-11-01 09:28:42 +0100

    0.3.1a2

--- AlxPackage/__init__.py
+++ AlxPackage/__init__.py
@@ -1,12 +1,11 @@
 bl_info = {
     "name" : "AlxOverHaul",
-    "author" : "Valery[.V Arhal]",
+    "author" : "Valery [.V Arhal]",
     "description" : "",
-    "version" : (0, 2, 0),
-    "warning" : "[Heavly(Not Frequently) Under Development] Minimum Supported BL Version 3.0",
-    "description": "An Addon To Simplify Workflow By Reducing The Number of Click To Do Stuff, And More",
+    "version" : (0, 3, 1),
+    "warning" : "[Heavly Under Development] Minimum Supported BL Version 3.0",
     "category" : "3D View",
-    "location" : "[Alx 3D] Panel Tab, Changes Based on Active Mode And Other",
+    "location" : "[Ctrl Alt A] PieMenu, [Alx 3D] Panel Tab",
     "blender" : (3, 6, 2)
 }
 
@@ -18,7 +17,7 @@ if "bpy" in locals():
 import bpy
 from bpy.types import Context
 import AlxPackage
-from bpy.props import StringProperty, FloatProperty, BoolProperty
+from bpy.props import StringProperty, IntProperty, FloatProperty, BoolProperty
 
 
 class Alx_MT_AlexandriaToolPie(bpy.types.Menu):
@@ -48,7 +47,8 @@ class Alx_MT_AlexandriaToolPie(bpy.types.Menu):
         PieUI.box().label(text = "PLACEHOLDER")
 
         RBoxMenuSpace = PieUI.box()
-        OT_MBSelection = RBoxMenuSpace.row().operator(Alx_OT_ModifierBevelSelection.bl_idname, text="Bevel Object")
+        OT_MBSelection = RBoxMenuSpace.row().operator(Alx_OT_ModifierBevelSelection.bl_idname, text="Bevel")
+        OT_MSSelection = RBoxMenuSpace.row().operator(Alx_OT_ModifierSubdivisionSelection.bl_idname, text="SubDivision")
 
         BBoxMenuSpace = PieUI.box()
 
@@ -219,8 +219,10 @@ class Alx_OT_ModifierBevelSelection(bpy.types.Operator):
     bl_label = ""
     bl_idname = "alx.modifier_bevel_selection"
 
-    ChamferWidth : FloatProperty(name="Width", default=0.01000)
     ShouldOverride : BoolProperty(name="Should Override", default=True)
+
+    Segments : IntProperty(name="Segments", default=1)
+    Width : FloatProperty(name="Width", default=0.01000, unit="LENGTH")
     UseWeight : BoolProperty(name="Use Weight", default=True)
 
     @classmethod
@@ -235,28 +237,91 @@ class Alx_OT_ModifierBevelSelection(bpy.types.Operator):
                         objMODs = getattr(SelObj, "modifiers", [])
                         for objMOD in objMODs:
                             if (objMOD.type == "BEVEL"):
-                                objMOD.width = self.ChamferWidth
-                                objMOD.segments = 1
+
+                                objMOD.segments = self.Segments
+                                objMOD.width = self.Width
+                                
                                 match self.UseWeight:
                                     case True:
                                         objMOD.limit_method = "WEIGHT"
                                     case False:
                                         objMOD.limit_method = "ANGLE"
                                         objMOD.angle_limit = 30 * (3.14/180)
+
                                 objMOD.miter_outer = "MITER_ARC"
                                 objMOD.harden_normals = True
+
                     case False:
+
                         NewBMod = SelObj.modifiers.new("Bevel", "BEVEL")
-                        NewBMod.width = self.ChamferWidth
-                        NewBMod.segments = 1
+                        NewBMod.segments = self.Segments
+                        NewBMod.width = self.Width
+
                         match self.UseWeight:
                                     case True:
                                         NewBMod.limit_method = "WEIGHT"
                                     case False:
                                         NewBMod.limit_method = "ANGLE"
                                         NewBMod.angle_limit = 30 * (3.14/180)
+
                         NewBMod.miter_outer = "MITER_ARC"
                         NewBMod.harden_normals = True      
+        return {"FINISHED"}
+    
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+class Alx_OT_ModifierSubdivisionSelection(bpy.types.Operator):
+    """"""
+
+    bl_label = ""
+    bl_idname = "alx.modifier_subdivision_selection"
+
+    ShouldOverride : BoolProperty(name="Should Override", default=True)
+
+    UseSimple : BoolProperty(name="Simple", default=False)
+    ViewportLevel : IntProperty(name="Viewport", default=1)
+    RenderLevel : IntProperty(name="Render", default=1)
+    Complex : BoolProperty(name="Complex", default=True)
+
+    @classmethod
+    def poll(cls, context):
+        return True
+    
+    def execute(self, context):
+        for SelObj in bpy.context.selected_objects:
+            if (SelObj.type == "MESH"):
+                match self.ShouldOverride:
+                    case True:
+                        objMODs = getattr(SelObj, "modifiers", [])
+                        for objMOD in objMODs:
+                            if (objMOD.type == "SUBSURF"):
+
+                                match self.UseSimple:
+                                    case True:
+                                        objMOD.subdivision_type = "SIMPLE"
+                                    case False:
+                                        objMOD.subdivision_type = "CATMULL_CLARK"
+
+                                objMOD.levels = self.ViewportLevel
+                                objMOD.render_levels = self.RenderLevel
+                                objMOD.show_only_control_edges = self.Complex
+
+                    case False:
+
+                        NewBMod = SelObj.modifiers.new("Subdivision Surface", "SUBSURF")
+
+                        match self.UseSimple:
+                                    case True:
+                                        NewBMod.subdivision_type = "SIMPLE"
+                                    case False:
+                                        NewBMod.subdivision_type = "CATMULL_CLARK"
+
+                        NewBMod.levels = self.ViewportLevel
+                        NewBMod.render_levels = self.RenderLevel
+                        NewBMod.show_only_control_edges = self.Complex
+
         return {"FINISHED"}
     
 
@@ -362,12 +427,7 @@ bpy.app.handlers.depsgraph_update_post.append(AlxUpdateActionUI)
 
 
 
-def register():
-    AlxPackage.AlxFeedToRegister()
-    AlxPackage.AlxFeedKeymaps()
 
-def unregister():
-    AlxPackage.AlxFeedToUnregister()
 
 AlxClassQueue = [
                 Alx_MT_AlexandriaToolPie,
@@ -378,45 +438,44 @@ AlxClassQueue = [
                 Alx_OT_ModeWeightPaintSwitch,
 
                 Alx_OT_ModifierBevelSwitch,
+                
                 Alx_OT_ModifierBevelSelection,
-
-                Alx_PT_PanelIDMapping,
-
-
-
-
-
-                Alx_Panel_SwapArmatureAction,
-                Alx_UL_ActionSelector
+                Alx_OT_ModifierSubdivisionSelection,
                 ]
 
-def AlxFeedToRegister():
+addon_keymaps = []
+
+def register():
     for AlxQCls in AlxClassQueue:
         bpy.utils.register_class(AlxQCls)
 
-    AlxFeedKeymaps()
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
 
-    bpy.types.Object.UIActionIndex = bpy.props.IntProperty(update=AlxUpdateAddonActionList)
+    km = kc.keymaps.new(name='3D View')
+    kmi = km.keymap_items.new(idname="wm.call_menu_pie", type="A", value="PRESS", ctrl=True, shift=False, alt=True, head=True)
+    kmi.properties.name = Alx_MT_AlexandriaToolPie.bl_idname
+
+    addon_keymaps.append((km,kmi))
+
+    #bpy.types.Object.UIActionIndex = bpy.props.IntProperty(update=AlxUpdateAddonActionList)
     print("AlxRegister Called")
     print("ALX_LIBRARY_LOADED")
     print(AlxClassQueue)
 
-def AlxFeedToUnregister():
+def unregister():
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    if kc:
+        for km, kmi in addon_keymaps:
+            km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
+
     for AlxQCls in AlxClassQueue:
         bpy.utils.unregister_class(AlxQCls)
 
-    del bpy.types.Object.UIActionIndex
+    #del bpy.types.Object.UIActionIndex
     print("AlxUnRegister Called")
-
-def AlxFeedKeymaps():
-    WindowMNG = bpy.context.window_manager
-    AlxAddonKeymapConfig = WindowMNG.keyconfigs.addon
-    
-    AlexandriaToolPieKM = AlxAddonKeymapConfig.keymaps.new(name="3D View", space_type="VIEW_3D")
-    ATPKeymapItem = AlexandriaToolPieKM.keymap_items.new("wm.call_menu_pie", ctrl=True, alt=True, type="A", value="PRESS")
-    ATPKeymapItem.properties.name = Alx_MT_AlexandriaToolPie.bl_idname
-
-
 
 if __name__ == "__main__":
     register()
