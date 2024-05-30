86db7cc8aeb1292860211ac96c5248db8adc0e97
Valery-AA <valeryatart@gmail.com>
2024-05-30 22:48:54 +0200

    v0.6.0.2

--- addon_auto_zipper.ps1
+++ addon_auto_zipper.ps1
@@ -1,9 +1,9 @@
 $parent_directory = Split-Path -Path (Get-Location) -Leaf -Resolve
 $target_subfolder = Split-Path -Path ".\$parent_directory" -Leaf -Resolve
 
-if ( Test-Path ".\$target_subfolder\__pycache__" )
+if ( get-childitem -Include __pycache__ -Recurse -force )
 {
-    Remove-Item  -Recurse -Path ".\$target_subfolder\__pycache__"
+    get-childitem -Include __pycache__ -Recurse -force | Remove-Item -Force -Recurse
 }
 if ( Test-Path ".\$target_subfolder\alxoverhaul_updater" )
 {
